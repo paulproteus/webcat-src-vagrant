71e2b482d96de85bd1e04df0da18e3b50ac970f2
Max Khon <fjoe@samodelkin.net>
2015-07-06 19:35:37 +0600

    Configure build environment for Web-CAT.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+export HOME=/home/vagrant
 
 if [ ! -f /usr/bin/javac ] ; then
     # Need to update or else the installs won't work
@@ -8,16 +9,49 @@ if [ ! -f /usr/bin/javac ] ; then
     sudo DEBIAN_FRONTEND=noninteractive apt-get install --force-yes -y tzdata/trusty
 
     # Some things we need to build Web-CAT.
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openjdk-7-jre openjdk-7-jdk cvs build-essential
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openjdk-7-jre openjdk-7-jdk ant git
 fi
 
+# install WebObjects
+[ -f WOInstaller.jar ] || curl -# -O http://wocommunity.org/tools/WOInstaller.jar
+wodir=/Library/WebObjects/Versions/WebObjects543
+sudo mkdir -p $wodir
+[ -d $wodir/Library/Frameworks/JavaXML.framework ] || sudo java -jar WOInstaller.jar 5.4.3 $wodir
+
+# install Wonder Frameworks
+[ -f Wonder-Frameworks.tar.gz ] || curl -# -O -L https://jenkins.wocommunity.org/job/Wonder/lastSuccessfulBuild/artifact/Root/Roots/Wonder-Frameworks.tar.gz
+sudo tar xfz Wonder-Frameworks.tar.gz -C $wodir/Library/Frameworks
+
+# woproject.jar
+[ -f woproject.jar ] || curl -# -O http://webobjects.mdimension.com/hudson/job/WOLips36Stable/lastSuccessfulBuild/artifact/woproject.jar
+mkdir -p ~/.ant/lib
+cp woproject.jar ~/.ant/lib
+
+# wobuild.properties
+mkdir -p ~/Library/Frameworks
+cat >~/Library/wobuild.properties << EOF
+wo.wosystemroot=$wodir
+wo.woroot=$wodir
+wo.user.frameworks=$HOME/Library/Frameworks
+wo.system.frameworks=$wodir/Library/Frameworks
+wo.bootstrapjar=$wodir/Library/WebObjects/JavaApplications/wotaskd.woa/WOBootstrap.jar
+wo.network.frameworks=/Network/Library/Frameworks
+wo.api.root=/Library/WebObjects/ADC%20Reference%20Library/documentation/WebObjects/Reference/API
+wo.network.root=/Network
+wo.extensions=$wodir/Local/Library/WebObjects/Extensions
+wo.user.root=$HOME
+wo.local.frameworks=$wodir/Local/Library/Frameworks
+wo.dir.local.library.frameworks=$wodir/Local/Library/Frameworks
+wo.apps.root=$wodir/Local/Library/WebObjects/Applications
+wo.wolocalroot=$wodir
+wo.dir.user.home.library.frameworks=$HOME/Library/Frameworks
+EOF
+sudo chown -R vagrant $wodir/Library/Frameworks ~/Library
+
 # If the source isn't checked out, check it out.
-if [ ! -d /vagrant/source ] ; then
-    mkdir -p /vagrant/source
-    pushd /vagrant/source
-    # CVS login, hilarious.
-    cvs  -d:pserver:anonymous:@web-cat.cvs.sourceforge.net:/cvsroot/web-cat login
-    # CVS checkout, hilarious.
-    cvs -z3 -d:pserver:anonymous@web-cat.cvs.sourceforge.net:/cvsroot/web-cat co -P Web-CAT
-    popd
+if [ ! -d web-cat ] ; then
+    mkdir -p web-cat
+    git clone https://github.com/mkhon/web-cat web-cat
 fi
+
+#(cd web-cat/Web-CAT && ant install.subsystems.and.build)
