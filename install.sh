2449690ce7e3871820eeab09c4b536022c098c7c
Radu Hobincu <radu.hobincu@upb.ro>
2016-01-19 20:56:52 +0200

    Replaced echo with printf in install.sh and also enclosed all strings in single quotes.

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 cd `dirname $0`
 set -e
 
-printf "Looking for javac... "
+echo -n 'Looking for javac... '
 if [ ! -f /usr/bin/javac ] ; then
-    printf "... not found. Installing packages... "
+    echo -n '... not found. Installing packages... '
     # Need to update or else the installs won't work. Failing is not critical.
     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update
 
@@ -14,37 +14,37 @@ if [ ! -f /usr/bin/javac ] ; then
     # Some things we need to build Web-CAT.
     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install -y openjdk-7-jre openjdk-7-jdk ant git || (echo Failed to install required packages ; exit 1)
 fi
-echo Done!
+echo 'Done!'
 
 fetch()
 {
 	url="$1"
 	file="`basename $url`"
 
-	curl -sSL "$url" -o "$file.tmp" && mv "$file.tmp" "$file" || (echo Failed to download $file. ; exit 1)
+	curl -sSL "$url" -o "$file.tmp" && mv "$file.tmp" "$file" || (echo 'Failed to download $file.' ; exit 1)
 }
 
 # install WebObjects
-printf "Installing WebObjects (root access required)... "
+echo -n 'Installing WebObjects (root access required)... '
 [ -f WOInstaller.jar ] || fetch http://wocommunity.org/tools/WOInstaller.jar
 wodir=/Library/WebObjects/Versions/WebObjects543
 sudo mkdir -p $wodir
-[ -d $wodir/Library/Frameworks/JavaXML.framework ] || (sudo java -jar WOInstaller.jar 5.4.3 $wodir >/dev/null || (sudo rm -rf $wodir && echo Unable to install WebObjects. && exit 1))
-echo Done!
+[ -d $wodir/Library/Frameworks/JavaXML.framework ] || (sudo java -jar WOInstaller.jar 5.4.3 $wodir >/dev/null || (sudo rm -rf $wodir && echo 'Unable to install WebObjects.' && exit 1))
+echo 'Done!'
 
 # install Wonder Frameworks
 printf "Installing Wonder Frameworks (root access required)... "
 [ -f Wonder-Frameworks.tar.gz ] || fetch https://github.com/wocommunity/wonder/releases/download/wonder-6.1.4/Wonder-Frameworks.tar.gz
 sudo tar xfz Wonder-Frameworks.tar.gz -C $wodir/Library/Frameworks
-echo Done!
+echo 'Done!'
 
 # woproject.jar is in the repository.
-# [ -f woproject.jar ] || fetch http://webobjects.mdimension.com/hudson/job/WOLips36Stable/lastSuccessfulBuild/artifact/woproject.jar
+[ -f woproject.jar ] || fetch http://webobjects.mdimension.com/hudson/job/WOLips36Stable/lastSuccessfulBuild/artifact/woproject.jar
 mkdir -p ~/.ant/lib
 cp woproject.jar ~/.ant/lib
 
 # wobuild.properties
-printf "Creating wobuild.properties... "
+echo -n 'Creating wobuild.properties... '
 mkdir -p ~/Library/Frameworks
 cat >~/Library/wobuild.properties << EOF
 wo.wosystemroot=$wodir
@@ -64,13 +64,13 @@ wo.wolocalroot=$wodir
 wo.dir.user.home.library.frameworks=$HOME/Library/Frameworks
 EOF
 sudo chown -R $USER $wodir/Library/Frameworks
-echo Done!
+echo 'Done!'
 
 # Retrieve the submodules
-printf "Retrieving submodules... "
+echo -n 'Retrieving submodules... '
 git submodule init
 git submodule update
-echo Done!
+echo 'Done!'
 
 if [ "$#" -eq 0 ] || [ ! "$1" = "--install-only" ]; then
     read -p "Do you want to build Web-CAT? (y/n) " -n 1 -r
