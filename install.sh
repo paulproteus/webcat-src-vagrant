e6e25e9be205b17f6874979fe2757b64f5d79dab
Radu Hobincu <radu.hobincu@upb.ro>
2016-01-22 00:50:12 +0200

    Eclosed  in quotes to avoid errors if it will ever contain spaces. Replaced a leftover printf with echo -n.

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ if [ ! -f /usr/bin/javac ] ; then
     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update
 
     # Downgrade tzdata, hilarious.
-    sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install --force-yes -y tzdata/trusty || (echo Failed to install tzdata. ; exit 1)
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install --force-yes -y tzdata/trusty || (echo 'Failed to install tzdata.' ; exit 1)
 
     # Some things we need to build Web-CAT.
-    sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install -y openjdk-7-jre openjdk-7-jdk ant git || (echo Failed to install required packages ; exit 1)
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install -y openjdk-7-jre openjdk-7-jdk ant git || (echo 'Failed to install required packages.' ; exit 1)
 fi
 echo 'Done!'
 
@@ -28,14 +28,14 @@ fetch()
 echo -n 'Installing WebObjects (root access required)... '
 [ -f WOInstaller.jar ] || fetch http://wocommunity.org/tools/WOInstaller.jar
 wodir=/Library/WebObjects/Versions/WebObjects543
-sudo mkdir -p $wodir
-[ -d $wodir/Library/Frameworks/JavaXML.framework ] || (sudo java -jar WOInstaller.jar 5.4.3 $wodir >/dev/null || (sudo rm -rf $wodir && echo 'Unable to install WebObjects.' && exit 1))
+sudo mkdir -p "$wodir"
+[ -d "$wodir/Library/Frameworks/JavaXML.framework" ] || (sudo java -jar WOInstaller.jar 5.4.3 "$wodir" >/dev/null || (sudo rm -rf "$wodir" && echo 'Unable to install WebObjects.' && exit 1))
 echo 'Done!'
 
 # install Wonder Frameworks
-printf "Installing Wonder Frameworks (root access required)... "
+echo -n 'Installing Wonder Frameworks (root access required)... '
 [ -f Wonder-Frameworks.tar.gz ] || fetch https://github.com/wocommunity/wonder/releases/download/wonder-6.1.4/Wonder-Frameworks.tar.gz
-sudo tar xfz Wonder-Frameworks.tar.gz -C $wodir/Library/Frameworks
+sudo tar xfz Wonder-Frameworks.tar.gz -C "$wodir/Library/Frameworks"
 echo 'Done!'
 
 # woproject.jar is in the repository.
@@ -63,7 +63,7 @@ wo.apps.root=$wodir/Local/Library/WebObjects/Applications
 wo.wolocalroot=$wodir
 wo.dir.user.home.library.frameworks=$HOME/Library/Frameworks
 EOF
-sudo chown -R $USER $wodir/Library/Frameworks
+sudo chown -R $USER "$wodir/Library/Frameworks"
 echo 'Done!'
 
 # Retrieve the submodules
